e6c8d2982e6f2f0079170ca41faf41a37f2afead
Paul <paulc2017@att.net>
2020-05-27 21:42:12 -0400

    Chapt 30 - More Angular

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
-
+   inactiveSilver: boolean = false;
+   inactiveBronze: boolean = false;
+   changeLoc: boolean = true;
    constructor() { }
 
    ngOnInit() { }
+
+   reactivateButtons() {
+      this.inactive = false;
+      this.inactiveSilver = false;
+      this.inactiveBronze = false;
+
+   }
 
 }
